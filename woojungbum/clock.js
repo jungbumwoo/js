466bfdc122543c6ea27ff1b48183451dfbe98516
jungbumwoo <jungbumwoo@naver.com>
2019-04-10 16:43:34 +0900

    Add files via upload

--- woojungbum/clock.js
+++ woojungbum/clock.js
@@ -1,3 +1,18 @@
-function init() {}
+const clockContainer = documnet.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+	const date = new Date();
+	const minutes = date.getMinutes();
+	const hours = date.getHours();
+	clockTitle.innerText = `${hours}:${minutes}`;
+
+};	
+
+
+function init() {
+	getTime();
+	setInterval(getTime, 1000);
+}
 
 init();
